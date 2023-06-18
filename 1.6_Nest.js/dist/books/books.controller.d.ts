f70ef4452426fc95d9333ede304b985154e12d62
Galentina <gala.malareva@gmail.com>
2023-06-18 13:57:49 +0200

    feat: create 1.8 task

--- 1.6_Nest.js/dist/books/books.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { BooksService } from './books.service';
-import { CreateBooksDto } from './dto/create-books.dto';
-import { BookInterface } from 'src/books/interfaces/book.interface';
-export declare class BooksController {
-    private readonly bookService;
-    constructor(bookService: BooksService);
-    getBooks(): Promise<BookInterface[]>;
-    createBook(book: CreateBooksDto): void;
-}
--- 1.6_Nest.js/dist/books/books.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { BookInterface } from 'src/books/interfaces/book.interface';
-export declare class BooksService {
-    private readonly books;
-    create(book: BookInterface): void;
-    getBooks(): BookInterface[];
-}
